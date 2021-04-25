65b1606b4f33857a4cfbf51c3d40f1ac73d4e4d8
Kristyn Little <kristynlittle@frontier.com>
2021-04-25 03:15:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,12 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    if @@genres.each do |genre|
-      genre_count[genre] += 1
-    else genre_count[genre] = 1
+      @@genres.each do |genre|
+        if genre_count[genre]
+          genre_count[genre] += 1
+        else 
+          genre_count[genre] = 1
+      end
     end
   end
 end
